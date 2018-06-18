408a52e8eb737556ab40e7559e49db454a439fe6
Shannan Kammann <sbkammann@gmail.com>
2018-06-17 17:39:56 -0700

    Create makeGrid() function

--- design.js
+++ design.js
@@ -3,6 +3,19 @@
 
 // When size is submitted by the user, call makeGrid()
 
+const pixelCanvas = document.querySelector('#pixelCanvas');
+let height = document.querySelector('#inputHeight');
+let width = document.querySelector('#inputWidth');
+
 function makeGrid() {
-  // Your code goes here!
+
+  for (let h = 1; h <= height; h++){
+    const row = document.createElement("tr");
+    pixelCanvas.appendChild(row);
+    for (let w = 1; w <= width; w++){
+      const column = document.createElement("td");
+      row.appendChild(column);
+    }
+  }
+
 }
